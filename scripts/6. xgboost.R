ed7a94dc8da312041e9e0314b6a27c15f951053f
Juancamilo0104 <112779729+Juancamilo0104@users.noreply.github.com>
2022-11-14 18:54:08 -0500

    Update 6. xgboost.R

--- scripts/6. xgboost.R
+++ scripts/6. xgboost.R
@@ -5,5 +5,3 @@ xgbtrain <- xgb.DMatrix(label = train$price, data=data.matrix(train[,c("surface_
 xgb = xgb.train(data = xgbtrain, max.depth = 3, nrounds = 100)
 
 pred_xgb <- predict(xgb, data.matrix(test[,c("surface_total2", "bedrooms", "bathrooms", "distancia_parque", "distancia_estacion_bus", "distancia_hospital", "distancia_estacion_policia")]))
-
-test$price <- pred_xgb
